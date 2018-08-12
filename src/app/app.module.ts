8567b90abaaa5ccdd50e8b27695d93f18969ab08
TwoDCube <2dcube01@gmail.com>
2018-08-13 00:22:33 +0200

    feat: Include library in app
    Signed-off-by: TwoDCube <2dcube01@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgxCalendarLibModule} from '../../projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxCalendarLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
