ef8c31fa0dc49ec1138a226f171780a6858c4787
TheEnd023 <sztibi023@gmail.com>
2018-08-19 13:37:13 +0200

    WIP

--- projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.component.ts
+++ projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChildren, AfterViewInit, ViewChild } from '@angular/core';
-import { Cell, Event } from './ngx-calendar-lib.interfaces';
+import { Cell, Event, DisplayedEvent } from './ngx-calendar-lib.interfaces';
 import { getDay, lastDayOfMonth, startOfWeek, addDays, subMonths, startOfMonth, getDate, addMonths, getDaysInMonth, format, isToday, parse, getMonth, isBefore, isAfter, isEqual, differenceInDays, isSunday, endOfMonth, endOfWeek, subDays } from 'date-fns';
 
 @Component({
@@ -14,7 +14,7 @@ export class NgxCalendarLibComponent implements OnInit, AfterViewInit {
 
   private _events: Event[] = [];
 
-  private _events2 = [];
+  private _displayedEvents: DisplayedEvent[] = [];
 
   constructor() { }
 
@@ -28,84 +28,51 @@ export class NgxCalendarLibComponent implements OnInit, AfterViewInit {
       asd.push(new Event(0, 'asd1', 'asdasd', new Date(), addDays(new Date(), 0), '#0B8043'));
       asd.push(new Event(1, 'asd2', 'asdasd', new Date(), addDays(new Date(), 1), '#D50000'));
       this.setEvents(asd);
-      console.log(this._events2);
+      console.log(this._displayedEvents);
     });
   }
 
 
-  private _generateEvents(){
-    this._events2 = [];
+  private _generateEvents(): void{
+    this._clearDisplayedEvents();
     for (const item of Object.keys(this._events)) {
       if (isBefore(this._events[item].startDate, this._events[item].endDate)) {
         let events = [];
         let eventStartDate = this._events[item].startDate;
-        for (let i = 0; i <= Math.abs(differenceInDays(this._events[item].startDate, this._events[item].endDate)); i++) {
+        for (let i = 0; i <= this._eventLenght(this._events[item]); i++) {
           if(isSunday(addDays(this._events[item].startDate, i)) && (format(addDays(this._events[item].startDate, i), 'YYYY-MM-DD') !== format(this._events[item].endDate, 'YYYY-MM-DD'))){
             events.push({id: this._events[item].id, title: this._events[item].title, startDate: eventStartDate, endDate: addDays(this._events[item].startDate, i), color: this._events[item].color})
             eventStartDate = addDays(this._events[item].startDate, i + 1);
           }
         } 
         events.push({id: this._events[item].id, title: this._events[item].title, startDate: eventStartDate, endDate: this._events[item].endDate, color: this._events[item].color})
-        this._events2.push(...events);
+        this._displayedEvents.push(...events);
       }else if(isEqual(this._events[item].startDate, this._events[item].endDate)){
-        this._events2.push({id: this._events[item].id, title: this._events[item].title, startDate: this._events[item].startDate, endDate: this._events[item].endDate, color: this._events[item].color});
+        this._displayedEvents.push({id: this._events[item].id, title: this._events[item].title, startDate: this._events[item].startDate, endDate: this._events[item].endDate, color: this._events[item].color});
       }
     }
     this._renderEvents();
   }
 
-  private _renderEvents(){
-    let firstDay = addDays(startOfWeek(lastDayOfMonth(subMonths(this.date, 1))), 1);
-
-    if (getDay(lastDayOfMonth(subMonths(this.date, 1))) === 0) {
-      firstDay = startOfMonth(this.date);
-    }
-
-    for (const item of this._events2) {
-      if(isAfter(item.startDate, subDays(firstDay, 1)) && isBefore(item.endDate, addDays(endOfWeek(endOfMonth(this.date)), 1))){
+  private _renderEvents(): void{
+    const firstCellDate = this._getFirstCellDate();
+    for (const item of this._displayedEvents) {
+      if(this._ifEventInMonth(item)){
         if (isBefore(item.startDate, item.endDate)) {
-          let rows = [];
-          for (let i = 0; i <= Math.abs(differenceInDays(item.startDate, item.endDate)); i++) {
-            const cell = this._cells[Math.abs(differenceInDays(addDays(parse(item.startDate), 1), firstDay))];
-            rows.push(cell.firstFreeRow);
-          }
-          let row = Math.max(...rows);
-          for (let i = 0; i <= Math.abs(differenceInDays(item.startDate, item.endDate)); i++) {
-            const cell = this._cells[Math.abs(differenceInDays(addDays(parse(item.startDate), i), firstDay))];
-            console.log(cell);
-            if (i === 0) {
-              cell.push(row, item.title, Math.abs(differenceInDays(item.startDate, item.endDate)) + 1, item.color);
-            } else {
-              cell.push(row, '', 0, '');
-            }
-          }
-        } else if (isEqual(format(item.startDate, 'YYYY-MM-DD'), format(item.endDate, 'YYYY-MM-DD'))) {
-          const cell = this._cells[Math.abs(differenceInDays(parse(item.startDate), firstDay))];
+          const row = this._getRow(item);
+          this._fillCellPlaceholder(item, row);
+        } else if (this._isEventOneDay(item)) {
+          const cell = this._cells[Math.abs(differenceInDays(parse(item.startDate), firstCellDate))];
           cell.push(cell.firstFreeRow, item.title, 1, item.color);
         }
       }
     }
   }
 
-  private _changeMonth() {
-    let rows = 5;
-    this._cells = [];
-    let firstDay = addDays(startOfWeek(lastDayOfMonth(subMonths(this.date, 1))), 1);
-    let prevMonth = getDaysInMonth(subMonths(this.date, 1)) - getDate(firstDay);
-
-    if (getDay(lastDayOfMonth(subMonths(this.date, 1))) === 0) {
-      firstDay = startOfMonth(this.date);
-      prevMonth = 0;
-    }
-
-    if ((getDay(startOfMonth(this.date)) === 0 && getDaysInMonth(this.date) >= 30) || (getDay(startOfMonth(this.date)) === 6 && getDaysInMonth(this.date) === 31)) {
-      rows = 6;
-    }
-
-    for (let i = 0; i < 7 * rows; i++) {
-      this._cells.push(new Cell(getDate(addDays(firstDay, i)), isToday(addDays(firstDay, i)), addDays(firstDay, i)));
-    };
-
+  private _changeMonth():void {
+    this._clearCells();
+    let rows = this._getRowsInMonth();
+    this._createCells(rows);
     this._renderEvents();
   }
 
@@ -126,11 +93,11 @@ export class NgxCalendarLibComponent implements OnInit, AfterViewInit {
     this._changeMonth();
   }
 
-  public nextMonth() {
+  public nextMonth():void {
     this.date = addMonths(this.date, 1);
   }
 
-  public prevMonth() {
+  public prevMonth():void {
     this.date = subMonths(this.date, 1);
   }
 
@@ -144,4 +111,68 @@ export class NgxCalendarLibComponent implements OnInit, AfterViewInit {
     this._generateEvents();
   }
 
+  private _getFirstCellDate(): Date {
+    if (isSunday(getDay(lastDayOfMonth(subMonths(this.date, 1))))) {
+      return startOfMonth(this.date);
+    }
+
+    return addDays(startOfWeek(lastDayOfMonth(subMonths(this.date, 1))), 1);
+  }
+
+  private _getRow(item: DisplayedEvent): number{
+    const firstCellDate = this._getFirstCellDate();
+    let rows = [];
+    for (let i = 0; i <= this._eventLenght(item); i++) {
+      const cell = this._cells[Math.abs(differenceInDays(addDays(parse(item.startDate), i), firstCellDate))];
+      rows.push(cell.firstFreeRow);
+    }
+    return Math.max(...rows);
+  }
+
+  private _fillCellPlaceholder(item: DisplayedEvent, row: number): void {
+    const firstCellDate = this._getFirstCellDate();
+    for (let i = 0; i <= this._eventLenght(item); i++) {
+      const cell = this._cells[Math.abs(differenceInDays(addDays(parse(item.startDate), i), firstCellDate))];
+      if (i === 0) {
+        cell.push(row, item.title, this._eventLenght(item)   + 1, item.color);
+      } else {
+        cell.push(row, '', 0, '');
+      }
+    }
+  }
+
+  private _ifEventInMonth(item: DisplayedEvent): boolean {
+    const firstCellDate = this._getFirstCellDate();
+    return isAfter(item.startDate, subDays(firstCellDate, 1)) && isBefore(item.endDate, addDays(endOfWeek(endOfMonth(this.date)), 1))
+  }
+
+  private _clearDisplayedEvents(): void {
+    this._displayedEvents = [];
+  }
+
+  private _createCells(rows: number): void {
+    const firstCellDate = this._getFirstCellDate();
+    for (let i = 0; i < 7 * rows; i++) {
+      this._cells.push(new Cell(getDate(addDays(firstCellDate, i)), isToday(addDays(firstCellDate, i)), addDays(firstCellDate, i)));
+    };
+  }
+
+  private _clearCells(): void {
+    this._cells = [];
+  }
+
+  private _getRowsInMonth(): number {
+    if ((getDay(startOfMonth(this.date)) === 0 && getDaysInMonth(this.date) >= 30) || (getDay(startOfMonth(this.date)) === 6 && getDaysInMonth(this.date) === 31)) {
+      return 6;
+    }
+    return 5;
+  }
+
+  private _isEventOneDay(item: DisplayedEvent): boolean {
+    return isEqual(format(item.startDate, 'YYYY-MM-DD'), format(item.endDate, 'YYYY-MM-DD'));
+  }
+
+  private _eventLenght(item: DisplayedEvent): number {
+    return Math.abs(differenceInDays(item.startDate, item.endDate));
+  }
 }
--- projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.interfaces.ts
+++ projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.interfaces.ts
@@ -1,113 +1,121 @@
 import { ElementRef } from "@angular/core";
 
 export class Event {
-    private _id: number;
-    private _title: string;
-    private _description?: string;
-    private _startDate: Date;
-    private _endDate?: Date;
-    private _color?: string;
+  private _id: number;
+  private _title: string;
+  private _description?: string;
+  private _startDate: Date;
+  private _endDate?: Date;
+  private _color?: string;
 
-    constructor(id: number, title: string, description: string, startDate: Date, endDate: Date, color: string) {
-        this._id = id;
-        this._title = title;
-        this._description = description || '';
-        this._startDate = startDate;
-        this._endDate = endDate || startDate;
-        this._color = color || '#ffdd00';
-    }
+  constructor(id: number, title: string, description: string, startDate: Date, endDate: Date, color: string) {
+    this._id = id;
+    this._title = title;
+    this._description = description || '';
+    this._startDate = startDate;
+    this._endDate = endDate || startDate;
+    this._color = color || '#ffdd00';
+  }
 
-    get id() {
-        return this._id;
-    }
-    get title() {
-        return this._title;
-    }
-    get description() {
-        return this._description;
-    }
-    get startDate() {
-        return this._startDate;
-    }
-    get endDate() {
-        return this._endDate;
-    }
-    get color() {
-        return this._color;
-    }
+  get id() {
+    return this._id;
+  }
+  get title() {
+    return this._title;
+  }
+  get description() {
+    return this._description;
+  }
+  get startDate() {
+    return this._startDate;
+  }
+  get endDate() {
+    return this._endDate;
+  }
+  get color() {
+    return this._color;
+  }
+}
+
+export interface DisplayedEvent {
+  id: number;
+  title: string;
+  startDate: Date;
+  endDate?: Date;
+  color?: string;
 }
 
 export class Cell {
-    private _rows: {
-        id: number;
-        free: boolean;
-        title: string;
-        width: number;
-        color: string;
-    }[] = [];
-    private _day: number;
-    private _date: Date;
-    private _today: boolean;
+  private _rows: {
+    id: number;
+    free: boolean;
+    title: string;
+    width: number;
+    color: string;
+  }[] = [];
+  private _day: number;
+  private _date: Date;
+  private _today: boolean;
 
-    constructor(day: number, today: boolean, date: Date) {
-        this._day = day;
-        this._today = today;
-        this._date = date;
-    }
+  constructor(day: number, today: boolean, date: Date) {
+    this._day = day;
+    this._today = today;
+    this._date = date;
+  }
 
-    get firstFreeRow(): number {
-        for (const item of this._rows) {
-            if (item.free) {
-                return item.id;
-            }
-        }
-        return this._rows.length;
+  get firstFreeRow(): number {
+    for (const item of this._rows) {
+      if (item.free) {
+        return item.id;
+      }
     }
+    return this._rows.length;
+  }
 
-    get day(): number {
-        return this._day;
-    }
+  get day(): number {
+    return this._day;
+  }
 
-    get today(): boolean {
-        return this._today;
-    }
+  get today(): boolean {
+    return this._today;
+  }
 
-    get date(): Date {
-        return this._date;
-    }
+  get date(): Date {
+    return this._date;
+  }
 
-    get events(){
-        let events = [];
-        for(const item of Object.keys(this._rows)){
-            if(!this._rows[item].free && this._rows[item].title !== ''){
-                let top = Number(item) * 24 + 'px';
-                let width = 'calc(' + this._rows[item].width*100 + '% + ' + (this._rows[item].width - 5) + 'px)';
-                events.push({title:this._rows[item].title, top: top, width: width, color: this._rows[item].color});
-            }
-        }
-        return events;
+  get events() {
+    let events = [];
+    for (const item of Object.keys(this._rows)) {
+      if (!this._rows[item].free && this._rows[item].title !== '') {
+        let top = Number(item) * 24 + 'px';
+        let width = 'calc(' + this._rows[item].width * 100 + '% + ' + (this._rows[item].width - 5) + 'px)';
+        events.push({ title: this._rows[item].title, top: top, width: width, color: this._rows[item].color });
+      }
     }
+    return events;
+  }
 
-    public push(row: number, title: string, width: number, color: string){
-        if (this._rows[row]) {
-            this._rows[row].free = false;
-            this._rows[row].title = title;
-            this._rows[row].width = width;
-            this._rows[row].color = color;
-        } else {
-            for (const _ of Array(row - this._rows.length + 1)) {
-                this._rows.push({
-                    id: this._rows.length,
-                    free: true,
-                    title: '',
-                    width: 0,
-                    color: ''
-                });
-            }
-            this._rows[row].free = false;
-            this._rows[row].title = title;
-            this._rows[row].width = width;
-            this._rows[row].color = color;
-        }
+  public push(row: number, title: string, width: number, color: string) {
+    if (this._rows[row]) {
+      this._rows[row].free = false;
+      this._rows[row].title = title;
+      this._rows[row].width = width;
+      this._rows[row].color = color;
+    } else {
+      for (const _ of Array(row - this._rows.length + 1)) {
+        this._rows.push({
+          id: this._rows.length,
+          free: true,
+          title: '',
+          width: 0,
+          color: ''
+        });
+      }
+      this._rows[row].free = false;
+      this._rows[row].title = title;
+      this._rows[row].width = width;
+      this._rows[row].color = color;
     }
+  }
 }
