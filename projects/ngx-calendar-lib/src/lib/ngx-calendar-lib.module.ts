2ff605a5f1908abfc37e19c2a035ff1abe37980b
TheEnd023 <sztibi023@gmail.com>
2018-08-27 15:52:35 +0200

    style: lint

--- projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.module.ts
+++ projects/ngx-calendar-lib/src/lib/ngx-calendar-lib.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
 import { NgxCalendarLibComponent } from './ngx-calendar-lib.component';
 
 @NgModule({
